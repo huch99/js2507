245576529d5d2d6ad255d6dd6810d3ae1be89840
huch99 <huichan58@naver.com>
2025-07-29 17:28:24 +0900

    0729 재수정

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let myData = [
+    {link: "./s02_js/day1_0724/index0724.html", day: "1"},
+    {link: "./s02_js/day2_0725/index0725.html", day: "2"},
+    {link: "./s02_js/day3_0728/index0728.html", day: "3"},
+    {link: "./s02_js/day4_0729/index0729.html", day: "4"}
+];
+
+for (let i=0; i < myData.length; i++) {
+    document.querySelector('#myData').innerHTML
+    += `<li><a href=${myData[i].link} target='_blank'>${myData[i].day}일차 js</a></li>`;
+}
