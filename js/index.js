047d01906b0346423436c4173744607c57a4128e
huch99 <huichan58@naver.com>
2025-07-30 17:40:32 +0900

    0730 수정

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ let myData = [
     {link: "./s02_js/day2_0725/index0725.html", day: "2"},
     {link: "./s02_js/day3_0728/index0728.html", day: "3"},
     {link: "./s02_js/day4_0729/index0729.html", day: "4"},
-    {link: "./s02_js/day4_0729/index0730.html", day: "5"}
+    {link: "./s02_js/day4_0730/index0730.html", day: "5"}
 ];
 
 for (let i=0; i < myData.length; i++) {
