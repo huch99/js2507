9e55c90985d1a097f1d8e9eded6a8f97e1799ad0
huch99 <huichan58@naver.com>
2025-07-30 15:54:13 +0900

    0730 수정

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ let myData = [
     {link: "./s02_js/day1_0724/index0724.html", day: "1"},
     {link: "./s02_js/day2_0725/index0725.html", day: "2"},
     {link: "./s02_js/day3_0728/index0728.html", day: "3"},
-    {link: "./s02_js/day4_0729/index0729.html", day: "4"}
+    {link: "./s02_js/day4_0729/index0729.html", day: "4"},
+    {link: "./s02_js/day4_0729/index0730.html", day: "5"}
 ];
 
 for (let i=0; i < myData.length; i++) {
